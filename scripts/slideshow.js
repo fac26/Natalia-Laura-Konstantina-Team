bec508d041d40514f3eedfad1558697b743dc1de
Natalia Rusu <bonapartik@gmail.com>
2022-09-28 15:27:33 +0100

    add slideshow to main page
    Relates #9

---
+++ scripts/slideshow.js
@@ -0,0 +1,21 @@
+const slideShowContainer = document.querySelector('.slideshow-container');
+const slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  slideIndex++;
+  for (i = 0; i < slides.length; i++) {
+    
+    slides[i].classList.remove('fade');
+    slides[i].style.display = 'none';
+  }
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  slides[slideIndex - 1].classList.add('fade');
+
+  setTimeout(showSlides, 10000);
+}
