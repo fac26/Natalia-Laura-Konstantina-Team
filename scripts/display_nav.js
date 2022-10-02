03604b01e9b6e21bc9238ba5faf732159d269075
Natalia Rusu <bonapartik@gmail.com>
2022-10-02 09:42:01 +0100

    add style and listeners to navigation menu
    Relates #16

--- scripts/display_nav.js
+++ scripts/display_nav.js
@@ -1,59 +1,28 @@
 const navIconMobile = document.querySelector('.nav-icon-mob'); //will be added
 const navItemsUlEl = document.querySelector('.nav-bar--menu');
-const navLinkEls=navItemsUlEl.querySelectorAll('li');
-const navBar = document.querySelector('.nav-bar');
 
 const showHideMenuHandler = (ev) => {
   navItemsUlEl.classList.toggle('hidden');
-  if (navItemsUlEl.classList.contains('hidden')) {//span width style as open or closed
-    navIconMobile
-      .querySelectorAll(':nth-child(odd)')//Odd and even are keywords that can be used to match child elements whose index is odd or even
-      .forEach((el) => (el.style.width = '100%'));//css styling navbar in mobile version
+  if (navItemsUlEl.classList.contains('hidden')) {
+    navIconMobile.classList.toggle('expanded-menu');
   } else {
-    navIconMobile
-      .querySelectorAll(':nth-child(odd)')
-      .forEach((el) => (el.style.width = '50%'));
-  }
-};
-
-const addMobVersListeners = () => {
-  navIconMobile.classList.remove('hidden');
-  navItemsUlEl.classList.add('hidden');
-  navBar.classList.add('backdrop-menu');
-  for (const item of navLinkEls) {
-    item.addEventListener('click', showHideMenuHandler);
-  }
-};
-
-const removeMobVersListeners = () => {
-  navBar.classList.remove('backdrop-menu');
-  navIconMobile.classList.add('hidden');
-  navItemsUlEl.classList.remove('hidden');//in case they are hidden
-  for (const item of navLinkEls) {
-    item.removeEventListener('click', showHideMenuHandler);
+    navIconMobile.classList.add('expanded-menu');      
   }
 };
 
 const checkMediaQueryHandler = () => {
   const mediaQuery = window.matchMedia('(max-width: 600px)');
-  if (mediaQuery.matches && navBar.dataset.mob === 'false') {
-    navBar.dataset.mob = 'true';
-    addMobVersListeners();
-  } else if (mediaQuery.matches && navBar.dataset.mob === 'true') {
-    return;
-  }
-  if (!mediaQuery.matches && navBar.dataset.mob === 'true') {
-    navBar.dataset.mob = 'false';
-    removeMobVersListeners();
+  if (mediaQuery.matches) {    
+    navItemsUlEl.addEventListener('click', showHideMenuHandler);
+    navItemsUlEl.classList.add('hidden'); 
+    navIconMobile.classList.remove('expanded-menu');  
+  } else{
+    navItemsUlEl.removeEventListener('click', showHideMenuHandler);
+    navItemsUlEl.classList.remove('hidden');    
   }
 };
 
-const checkMediaOnLoanding = () => {
-  const mediaQuery = window.matchMedia('(max-width: 600px)');
-  mediaQuery.matches
-    ? ((navBar.dataset.mob = 'true'), addMobVersListeners())
-    : (navBar.dataset.mob = 'false');
-};
-checkMediaOnLoanding();
+checkMediaQueryHandler();
+
 navIconMobile.addEventListener('click', showHideMenuHandler);
 window.addEventListener('resize', checkMediaQueryHandler);
