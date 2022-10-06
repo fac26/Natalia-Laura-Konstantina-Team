02d61d1562bd28992450c858dbaff5c87b18794d
LauraK0 <laurakeating@live.co.uk>
2022-10-06 16:15:47 +0100

    Add event listener and removed onsubmit
    Relates #35

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function validateMyForm() {
+document.getElementById("contactForm").addEventListener("submit", validateMyForm);
+
+function validateMyForm(e) {
     var name = document.forms.contactForm.name.value;
     var companyName = document.forms.contactForm.com.value;
     var email = document.forms.contactForm.email.value;
@@ -8,21 +10,25 @@ function validateMyForm() {
     var validateText = /\d+$/g; // Javascript reGex for Name validation
 
     if (name == '' || name.trim().length === 0 || validateText.test(name)) {
+      e.preventDefault();
       window.alert('Please tell us your name');
       return false;
     }
 
     if (companyName == '' || companyName.trim().length === 0 || validateText.test(companyName)) {
+      e.preventDefault();
       window.alert('Please tell us your Company\'s name');
       return false;
     }
 
     if (email == '' || !validateEmail.test(email)) {
+      e.preventDefault();
       window.alert('Please enter a valid e-mail address.');
       return false;
     }
 
     if (message == '' || message.trim().length === 0 || validateText.test(message)) {
+      e.preventDefault();
       window.alert('Please enter your message.');
       return false;
     }
